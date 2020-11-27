1a163756c638974080c6a072a655153e3413d046
Obada <obada.khalili@outlook.com>
2020-11-27 20:10:50 +0200

    Make navbar responsive

---
+++ public/script.js
@@ -0,0 +1,20 @@
+function main() {
+  const $sidebar = document.getElementById("sidebar");
+  const $sidebarChildrenWrapper = document.getElementById("sidebarChildrenWrapper");
+  const $sidebarOpenBtn = document.getElementById("sidebarOpenBtn");
+  const $sidebarCloseBtn = document.getElementById("sidebarCloseBtn");
+  
+  $sidebarOpenBtn.addEventListener("click", () => {
+    $sidebar.classList.add("active");
+    $sidebar.classList.remove("w-0");
+    setTimeout(() => $sidebarChildrenWrapper.classList.add("active"), 300);
+  });
+
+  $sidebarCloseBtn.addEventListener("click", () => {
+    $sidebarChildrenWrapper.classList.remove("active");
+    $sidebar.classList.add("w-0");
+    $sidebar.classList.remove("active");
+  });
+}
+
+window.addEventListener("load", main);
