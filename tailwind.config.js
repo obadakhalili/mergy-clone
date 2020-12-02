f6edb4ea19d0be6a0a71260d75d97c9050db3a3d
Ahmad Khalili <ahmadkhalili01@outlook.com>
2020-12-02 18:59:01 +0200

    Adopt tailwind built-in color palette, instead of extending custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@ module.exports = {
   purge: ["./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        "primary": "rgb(0, 123, 255)",
-        "secondary": "rgb(35, 35, 35)"
-      }
-    },
+    extend: {},
   },
   variants: {
     extend: {},
