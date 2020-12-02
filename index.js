e92da5f3ec1fc7ceee04c38c84e86a85816796d8
Obada <obada.khalili@outlook.com>
2020-12-02 10:06:16 +0200

    Add CSSO to the PostCSS process to minify resulted CSS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const postcss = require("postcss");
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
 const { promises: fs } = require("fs");
 
 (async () => {
@@ -8,7 +9,11 @@ const { promises: fs } = require("fs");
     console.log("Buidling ...");
 
     const postCSS = await fs.readFile("src/styles.css");
-    const result = await postcss([tailwindcss, autoprefixer]).process(postCSS, { from: "src/styles.css", to: "public/styles/index.css" });
+    const result = await postcss([
+      tailwindcss,
+      autoprefixer,
+      csso
+    ]).process(postCSS, { from: "src/styles.css", to: "public/styles/index.css" });
     await fs.writeFile("public/styles/index.css", result.css);
     
     console.log("The CSS file was built successfully");
