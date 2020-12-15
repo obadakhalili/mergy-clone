89731a8de8df629aa76610910920d3350363c6dd
Ahmad Khalili <ahmadkhalili01@outlook.com>
2020-12-15 19:06:24 +0200

    Define the content of the services component ;)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,25 +2,25 @@ const services = [
   {
     title: "Ease & Precision",
     icon: "share-2",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu dolor vitae metus placerat malesuada sit amet ut leo. Etiam aliquam pharetra nisl. Praesent pretium, eros ac maximus varius, mi purus dignissim ipsum, sit amet convallis tellus magna non risus. Suspendisse et odio mauris. Suspendisse nec magna vel lorem finibus tincidunt. Nulla quam velit, ornare ac mauris nec, ultricies facilisis diam. Vivamus vitae ipsum metus.",
+    desc: "We provide you with an easy to use UI. Hiring is detailed and optimized for every scenario and is viable with a few clicks. And we also produce an exact and honest salary rate that suits both the employer's and the employee's needs.",
     image: "connect"
   },
   {
     title: "Talent",
     icon: "circle",
-    desc: "Nulla bibendum velit non aliquam malesuada. Integer tincidunt elit gravida purus laoreet, non rhoncus lacus bibendum. Suspendisse potenti. Aenean enim velit, pretium vitae semper a, dictum vel augue. Cras et magna ac elit posuere congue nec et purus. Sed consequat nisl nec quam lobortis pharetra. Aenean viverra fringilla neque eu mattis. Suspendisse laoreet nibh eget lectus facilisis, vel pulvinar augue scelerisque. Cras facilisis feugiat dui, nec pretium orci dignissim aliquam. Aenean tincidunt lacinia tellus nec dignissim. Etiam turpis felis, convallis molestie enim nec, posuere fermentum mauris. Nulla tempus finibus velit at ullamcorper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam diam lorem, feugiat ac odio sit amet, tempor congue felis.",
+    desc: "Everyone wants an ideal employee. So, our platform connects with top-end software engineers. Most of which have a significant amount of skill sets along with real-world experience to suit everyone's requirements.",
     image: "talent"
   },
   {
     title: "Collaboration",
     icon: "users",
-    desc: "Suspendisse at risus massa. Nam dui turpis, dapibus et interdum quis, posuere interdum tellus. Suspendisse non ornare massa, et tempor felis. Ut vestibulum mauris lobortis commodo fringilla. Nullam cursus mi sed vehicula maximus. Cras eu maximus risus. Mauris quis felis viverra, fringilla neque nec, condimentum nisi.",
+    desc: "Even though our software engineers are talented as individuals, we decided to equip our clients with the choice of hiring teams, as some scenarios require the distribution of the workload over a group.",
     image: "collaboration"
   },
   {
     title: "Profit",
     icon: "dollar-sign",
-    desc: "Suspendisse vitae elementum massa. Morbi maximus magna ante, nec imperdiet dui rutrum ut. Sed vel tellus vestibulum, lobortis enim id, congue sem. Mauris ligula tortor, fringilla in placerat non, dictum luctus odio. Aliquam ut turpis turpis. Proin quis augue vel purus vestibulum vestibulum nec et lectus. Aliquam quis lacus felis. Suspendisse potenti.",
+    desc: "The salary rates we provide are precise and honest. Even though the employees we provide are top-end software engineers, the wages that accompany them are fair and well thought out to accommodate your company's profit and growth. To help boost your focus on your business logic, leaving the process of finding an ideal employee to us.",
     image: "money"
   }
 ];
